ccefb57a0d1c9995e4e14e34a7e168c0ca66124a
Elias Garza Valdés <eliasgarzav@gmail.com>
2023-06-19 18:26:42 -0600

    Added plots to UI

--- UI/Pages/general_info.py
+++ UI/Pages/general_info.py
@@ -2,6 +2,10 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
+import sys
+sys.path.append("../")
+
+from tools.Insights.dashboard import *
 
 st.set_page_config(page_title="General Insights",
                 #    page_icon= Image.open('boat-removebg-preview.png')
@@ -27,13 +31,17 @@ def cb():
     
 chat = st.session_state['chat']
 
-fig, ax = plt.subplots()
+with st.container():
+    st.pyplot(plot_number_texts(chat))
 
-num_chats = chat['name'].value_counts()
-ax.bar(num_chats.index, num_chats)
-plt.xticks(rotation='vertical')
-plt.xlabel('Names')
-plt.ylabel('Number of texts')
+col1, col2 = st.columns(2)
 
-st.pyplot(fig)
+with col1:
+    st.pyplot(plot_texts_per_weekday(chat))
+    st.pyplot(plot_texts_per_day(chat))
+    
+with col2:
+    st.pyplot(plot_texts_per_month(chat))
+    st.pyplot(plot_texts_per_hour(chat))
+    
 
